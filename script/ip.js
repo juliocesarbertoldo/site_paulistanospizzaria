3543a5d5c6327217b2935da196ede47abccce02f
Julio Cesar Bertoldo <juliocesar.bertt@gmail.com>
2023-08-10 15:59:57 -0300

    ,

--- script/ip.js
+++ script/ip.js
@@ -5,8 +5,8 @@ function carregar() {
 					return response.json();
 				})
 				.then((data) => {
-					if (data.ip == '177.8.250.214') {
-                        ip_sistema.src = 'http://192.168.1.100:58080'
+					if (data.ip == '177.8.250.2145') {
+                        ip_sistema.src = 'http://192.168.1.100/mmrest/autocliente/'
                     }
 				});
 }
